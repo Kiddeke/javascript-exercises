e1b211add11be2fd499e28f09c77f6e99bc34c1b
Grant Deker (WSL2) <117955357+Kiddeke@users.noreply.github.com>
2024-02-09 22:52:23 -0500

    Complete reverseString problem using array methods loops and string methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,7 +1,12 @@
-const reverseString = function() {
-let helloThere = "hello there";
-const reverse = helloThere.reverse();
-return reverse;
+const reverseString = function(string) {
+    let val = [];
+    let len = string.length - 1;
+    for (let i = len; i > -1; i--){
+        let char = string.charAt(i);
+        val.push(char);
+    }
+
+return val.join('');
 };
 
 // Do not edit below this line
