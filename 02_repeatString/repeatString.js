e525489d30875ec063f36b3110e615bebde0cc22
Grant Deker (WSL2) <117955357+Kiddeke@users.noreply.github.com>
2024-02-09 21:40:03 -0500

    First pass as repeat string loop problem

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,11 @@
-const repeatString = function() {
-const repeat = "hey";
-return repeat.repeat(3)
+const repeatString = function(string, num) {
+    for (let i = 0; i < num + 1; i++) {
+        return string
+    }
+return repeatString("hey", 3)
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
 const reverseString = function() {
-
+let helloThere = "hello there";
+const reverse = helloThere.reverse();
+return reverse;
 };
 
 // Do not edit below this line
