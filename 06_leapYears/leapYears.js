47a883d3db847d7523480b5299091055939022e2
Grant Deker (WSL2) <117955357+Kiddeke@users.noreply.github.com>
2024-02-12 20:01:38 -0500

    Complete leap year problem passing all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
 const leapYears = function(year) {
-return ((year % 4 === 0 && year % 100 !== %0)) ? true : false
-};
+        const leap = year % 4;
+        const leap2 = year % 100;
+        const leap3 = year % 400;
+        const leap4 = year % 2;
+        if (leap4 !== 0) {
+            return false;
+          } else if ((leap === 0) && ((leap2 === 0) && (leap3 !==0))) {
+             return false;
+            } else {
+              return true;
+             } 
+         };
 
 // Do not edit below this line
 module.exports = leapYears;
